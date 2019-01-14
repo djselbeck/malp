6c0f510eb90134b92d1f10f4cc2a2f5078674099
Frederik Lütkes <frederik.luetkes@googlemail.com>
2019-01-14 21:29:05 +0100

    Yearly copyright update

--- app/src/main/java/org/gateshipone/malp/mpdservice/handlers/responsehandler/MPDResponseAlbumArt.java
+++ app/src/main/java/org/gateshipone/malp/mpdservice/handlers/responsehandler/MPDResponseAlbumArt.java
@@ -1,9 +1,9 @@
 /*
- *  Copyright (C) 2018 Team Gateship-One
+ *  Copyright (C) 2019 Team Gateship-One
  *  (Hendrik Borghorst & Frederik Luetkes)
  *
  *  The AUTHORS.md file contains a detailed contributors list:
- *  <https://github.com/gateship-one/malp/blob/master/AUTHORS.md>
+ *  <https://gitlab.com/gateship-one/malp/blob/master/AUTHORS.md>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
